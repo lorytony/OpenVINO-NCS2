c291370bfffafbe19d01486706256b43e8a18685
Lorenzo Tonelli <Lorenzo.tonelli.d.a@gmail.com>
2023-03-13 18:27:47 +0100

    Add files via upload

---
+++ convert-model.py
@@ -0,0 +1,21 @@
+#import numpy as np
+import random as rn
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, models, optimizers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.applications import VGG16
+import PIL
+from PIL import Image
+import cv2
+import os
+import tf2onnx
+import onnx
+
+print("conversion")
+vgg_model = tf.keras.models.load_model('model/model.h5')
+#vgg_model = tf.keras.models.load_model('model/model.h5')
+
+#vgg_model.save('./vgg16net/') #it should save into pb formato
+onnx_model, _ = tf2onnx.convert.from_keras(vgg_model, opset=11)#opset=13
+onnx.save(onnx_model, "model-opset10.onnx")
